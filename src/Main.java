e5933bd36dea013b3778226b7562a9bb2951d833
Dhananjai Ghawalkar <dhananjaighawalkar68@gmail.com>
2026-06-26 12:01:11 +0530

    Retreived passengers detail

--- src/Main.java
+++ src/Main.java
@@ -15,46 +15,23 @@ public class Main {
         }
         try {
             Connection connection = DriverManager.getConnection(url,username,password);
-            Scanner s = new Scanner(System.in);
-            System.out.println("Give passenger name : ");
-            String p_name = s.next();
-            System.out.println("Give passenger age : ");
-            int p_age = s.nextInt();
-            System.out.println("Boarding point ?");
-            String boarding_point = s.next();
-            System.out.println("Destination : ");
-            String destination = s.next();
-            System.out.println("Date booked : ");
-            String date_booked = s.next();
-            System.out.println("Amount Paid : ");
-            double amount = s.nextDouble();
-            System.out.println("Provide Bus_ID : ");
-            int busId = s.nextInt();
-            String query = """
-        INSERT INTO booking
-        (passenger_name, passenger_age, boarding_point,
-        destination, date_booked, amount, bus_id)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-        """;
+
+            String query = "select * from booking join bus on booking.bus_id = bus.bus_id;";
 
             PreparedStatement ps = connection.prepareStatement(query);
 
-            ps.setString(1, p_name);
-            ps.setInt(2, p_age);
-            ps.setString(3, boarding_point);
-            ps.setString(4, destination);
-            ps.setDate(5, Date.valueOf(date_booked));
-            ps.setDouble(6, amount);
-            ps.setInt(7, busId);
-
-            int rowsAffected = ps.executeUpdate();
-
-            if (rowsAffected > 0) {
-                System.out.println("Booking Added Successfully.");
-            } else {
-                System.out.println("Booking Failed.");
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                System.out.println(rs.getString("passenger_name"));
+                System.out.println(rs.getInt("passenger_age"));
+                System.out.println(rs.getString("bus_number"));
+                System.out.println(rs.getString("destination"));
+                System.out.println("------------");
             }
 
+
+
+            rs.close();
             ps.close();
             connection.close();
         } catch (SQLException e) {
