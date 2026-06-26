001fa8f4b8fb8acf8ae052ba0d6120ff8c2966a5
Dhananjai Ghawalkar <dhananjaighawalkar68@gmail.com>
2026-06-26 09:49:53 +0530

    Initial Bus Exercise project setup

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    private static final String url = "jdbc:mysql://localhost:3306/BusExcercise";
+    static private final String username = "root";
+    private static final String password = "6309744577";
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
