70d1076095ecedcddb38a90b02976b67d9cd2868
Dhananjai Ghawalkar <dhananjaighawalkar68@gmail.com>
2026-06-26 11:30:23 +0530

    inserting data in booking table

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -14,30 +15,47 @@ public class Main {
         }
         try {
             Connection connection = DriverManager.getConnection(url,username,password);
-            Statement stmt = connection.createStatement();
-            String busTable = "CREATE TABLE bus (\n" +
-                    "    bus_id INT PRIMARY KEY AUTO_INCREMENT,\n" +
-                    "    bus_number VARCHAR(20) UNIQUE NOT NULL,\n" +
-                    "    starting_point VARCHAR(50) NOT NULL,\n" +
-                    "    ending_point VARCHAR(50) NOT NULL,\n" +
-                    "    travel_date DATE NOT NULL,\n" +
-                    "    capacity INT NOT NULL,\n" +
-                    "    available_seats INT NOT NULL\n" +
-                    ");";
-            stmt.executeUpdate(busTable);
-            String bookingTable = "CREATE TABLE booking (\n" +
-                    "    passenger_id INT PRIMARY KEY AUTO_INCREMENT,\n" +
-                    "    passenger_name VARCHAR(50) NOT NULL,\n" +
-                    "    passenger_age INT NOT NULL,\n" +
-                    "    boarding_point VARCHAR(50) NOT NULL,\n" +
-                    "    destination VARCHAR(50) NOT NULL,\n" +
-                    "    date_booked DATE NOT NULL,\n" +
-                    "    amount DECIMAL(10,2) NOT NULL,\n" +
-                    "    bus_id INT NOT NULL,\n" +
-                    "    FOREIGN KEY (bus_id) REFERENCES bus(bus_id)\n" +
-                    ");";
-            stmt.executeUpdate(bookingTable);
-            stmt.close();
+            Scanner s = new Scanner(System.in);
+            System.out.println("Give passenger name : ");
+            String p_name = s.next();
+            System.out.println("Give passenger age : ");
+            int p_age = s.nextInt();
+            System.out.println("Boarding point ?");
+            String boarding_point = s.next();
+            System.out.println("Destination : ");
+            String destination = s.next();
+            System.out.println("Date booked : ");
+            String date_booked = s.next();
+            System.out.println("Amount Paid : ");
+            double amount = s.nextDouble();
+            System.out.println("Provide Bus_ID : ");
+            int busId = s.nextInt();
+            String query = """
+        INSERT INTO booking
+        (passenger_name, passenger_age, boarding_point,
+        destination, date_booked, amount, bus_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """;
+
+            PreparedStatement ps = connection.prepareStatement(query);
+
+            ps.setString(1, p_name);
+            ps.setInt(2, p_age);
+            ps.setString(3, boarding_point);
+            ps.setString(4, destination);
+            ps.setDate(5, Date.valueOf(date_booked));
+            ps.setDouble(6, amount);
+            ps.setInt(7, busId);
+
+            int rowsAffected = ps.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Booking Added Successfully.");
+            } else {
+                System.out.println("Booking Failed.");
+            }
+
+            ps.close();
             connection.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
