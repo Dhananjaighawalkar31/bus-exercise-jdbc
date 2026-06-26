0cd26d81623b36b444b013e854d3abe508f3c94a
Dhananjai Ghawalkar <dhananjaighawalkar68@gmail.com>
2026-06-26 10:11:34 +0530

    created booking and bus table

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -10,6 +12,35 @@ public class Main {
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
-
+        try {
+            Connection connection = DriverManager.getConnection(url,username,password);
+            Statement stmt = connection.createStatement();
+            String busTable = "CREATE TABLE bus (\n" +
+                    "    bus_id INT PRIMARY KEY AUTO_INCREMENT,\n" +
+                    "    bus_number VARCHAR(20) UNIQUE NOT NULL,\n" +
+                    "    starting_point VARCHAR(50) NOT NULL,\n" +
+                    "    ending_point VARCHAR(50) NOT NULL,\n" +
+                    "    travel_date DATE NOT NULL,\n" +
+                    "    capacity INT NOT NULL,\n" +
+                    "    available_seats INT NOT NULL\n" +
+                    ");";
+            stmt.executeUpdate(busTable);
+            String bookingTable = "CREATE TABLE booking (\n" +
+                    "    passenger_id INT PRIMARY KEY AUTO_INCREMENT,\n" +
+                    "    passenger_name VARCHAR(50) NOT NULL,\n" +
+                    "    passenger_age INT NOT NULL,\n" +
+                    "    boarding_point VARCHAR(50) NOT NULL,\n" +
+                    "    destination VARCHAR(50) NOT NULL,\n" +
+                    "    date_booked DATE NOT NULL,\n" +
+                    "    amount DECIMAL(10,2) NOT NULL,\n" +
+                    "    bus_id INT NOT NULL,\n" +
+                    "    FOREIGN KEY (bus_id) REFERENCES bus(bus_id)\n" +
+                    ");";
+            stmt.executeUpdate(bookingTable);
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
